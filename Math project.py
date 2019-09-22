4605a8e74100f4ded4dc03403d1f06558b47b684
Liam Tyler <lgt2621@g.rit.edu>
2019-09-22 19:25:08 -0400

    Renamed variables to better names, made values work with any starting and ending point, and added comments

--- Math project.py
+++ Math project.py
@@ -1,16 +1,26 @@
 from scipy.integrate import quad
 from matplotlib import pyplot
-x=[]
-y=[]
+
+power_x_axis=[]
+Midpoint_ratio_y_axis=[]
+
 def values(partitions, start, end):
-    midpoint=[]
-    trapazoid=[]
+    """
+    Calculates the x values for the midpoint and trapezoidal sums
+    :param partitions: The number of partitions of the function
+    :param start: The starting value of the integral
+    :param end: The end value of the integral
+    :return: midpointx- the x values of the midpoint sum and trapezoidx- the x values of the trapezoidal sum
+    """
+    midpointx=[]
+    trapazoidx=[]
     for x in range(partitions):
-        midpoint.append(((end-start)/(2*partitions))+(.125*x))
+        midpointx.append((end-start)/(2*partitions)+((end-start)/partitions)*x)
     for x in range(2*partitions):
-        trapazoid.append(((end-start)/(2*partitions))*x)
-    trapazoid.append(end)
-    return midpoint, trapazoid
+        trapazoidx.append(((end-start)/(2*partitions))*x)
+    trapazoidx.append(end)
+    return midpointx, trapazoidx
+
 def fx(power, lst):
     values=[]
     for x in lst:
@@ -37,7 +47,7 @@ def main(power):
     print("power: x^",power,sep="")
     partitions=8
     start=0
-    end=1
+    end=4
     mid, trap=values(partitions, start, end)
     midval=fx(power, mid)
     trapval=fx(power, trap)
@@ -48,14 +58,14 @@ def main(power):
     act=inter(start, end, power)
     print("actual: ", act)
     trat, mrat=ratio(A, B, act)
-    x.append(power)
-    y.append(mrat)
+    power_x_axis.append(power)
+    Midpoint_ratio_y_axis.append(mrat)
     print(act," = ", mrat," Midpoint ", "+ ", trat, " Trapezoid", sep="")
-for i in range(2, 500): # 4 512 8
+for i in range(2, 3):
     main(i)
-print(x, "\n", y)
+print(power_x_axis, "\n", Midpoint_ratio_y_axis)
 
-pyplot.plot(x,y)
+pyplot.plot(power_x_axis, Midpoint_ratio_y_axis)
 pyplot.title('Extended Graph')
 pyplot.ylabel('Amplitude')
 pyplot.xlabel('Power of X')
